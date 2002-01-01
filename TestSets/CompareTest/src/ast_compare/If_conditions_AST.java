b16e87b82eb83d553bae291af0af6ac52d885b1f
artemgarishin <artem.garishin@gmail.com>
2002-01-01 14:27:39 +0100

    ast next

--- TestSets/CompareTest/src/ast_compare/If_conditions_AST.java
+++ TestSets/CompareTest/src/ast_compare/If_conditions_AST.java
@@ -42,4 +42,15 @@ public class If_conditions_AST {
 			System.out.println();
 		}
 	}
+	
+	public void test6(){
+		int o = 1;
+		int i = 1;
+		if(o >= 0 || o != 1){
+			System.out.println("123");
+		} else{
+			i++;
+		}
+		int p = 0;
+	}
 }
