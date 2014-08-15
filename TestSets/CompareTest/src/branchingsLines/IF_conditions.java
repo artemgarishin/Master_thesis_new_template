fbf1ddacff88cc826e737d6f499ee7c6a103db8e
artemgarishin <artem.garishin@gmail.com>
2014-08-15 16:36:38 +0200

    IF conditions added

---
+++ TestSets/CompareTest/src/branchingsLines/IF_conditions.java
@@ -0,0 +1,29 @@
+package branchingsLines;
+
+public class IF_conditions {
+
+	public void test1(){
+		int i = 0;
+		
+		if(i > 0){
+			System.out.println();
+		}
+	}
+	
+	public void test2(){
+		int i = 0;
+		
+		if(i < 0){
+			System.out.println();
+		}
+	}
+	
+	public void test3(){
+		int i = 0;
+		int k = 1;
+		
+		if(i < 0){
+			System.out.println();
+		}
+	}
+}
