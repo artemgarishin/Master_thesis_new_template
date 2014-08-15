ea2af18029ed622b678b727efab07ada92985122
artemgarishin <artem.garishin@gmail.com>
2014-08-15 13:59:52 +0200

    experiments sequential blocks, first steps

---
+++ TestSets/CompareTest/src/simpleBlocks/SequentialLines.java
@@ -0,0 +1,18 @@
+package simpleBlocks;
+
+public class SequentialLines {
+
+	public void test1(){
+		int i = 0;
+	}
+	
+	public void test2(){
+		int i = 0;
+		int k = 0;
+	}
+	
+	public void test3(){
+		int i = 0; int b = 1;
+	}
+	
+}
