6288239982d1335dd3d7498ef00006c2f7dd5b03
artemgarishin <artem.garishin@gmail.com>
2014-08-20 14:28:18 +0200

    code evaluation started

--- TestSets/CompareTest/src/branchingsLines/IF_conditions.java
+++ TestSets/CompareTest/src/branchingsLines/IF_conditions.java
@@ -20,9 +20,25 @@ public class IF_conditions {
 	
 	public void test3(){
 		int i = 0;
-		int k = 1;
-		
 		if(i < 0){
+			System.out.println();
+		}
+	}
+	
+	public void test4(){
+		int i = 0;
+		if(i < 0){
+			System.out.println();
+		} else{
+			System.out.println("123");
+		}
+	}
+	
+	public void test5(){
+		int o = 0;
+		if(o >= 0 || o != 1){
+			System.out.println("123");
+		} else{
 			System.out.println();
 		}
 	}
