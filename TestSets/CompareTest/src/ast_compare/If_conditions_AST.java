27b640c66fe6d55ab26317a5edfcb161e154f9b6
artemgarishin <artem.garishin@gmail.com>
2014-09-10 16:58:36 +0200

    ast further

---
+++ TestSets/CompareTest/src/ast_compare/If_conditions_AST.java
@@ -0,0 +1,45 @@
+package ast_compare;
+
+public class If_conditions_AST {
+
+	public void test1(){
+		int i = 0;
+		
+		if(i > 0){
+			System.out.println();
+		}
+	}
+	
+	public void test2(){
+		int i = 0;
+		
+		if(i < 0){
+			System.out.println();
+		}
+	}
+	
+	public void test3(){
+		int i = 0;
+		if(i < 0){
+			System.out.println();
+		}
+	}
+	
+	public void test4(){
+		int i = 0;
+		if(i < 0){
+			System.out.println();
+		} else{
+			System.out.println("123");
+		}
+	}
+	
+	public void test5(){
+		int o = 0;
+		if(o >= 0 || o != 1){
+			System.out.println("123");
+		} else{
+			System.out.println();
+		}
+	}
+}
---
+++ TestSets/CompareTest/src/ast_compare/SequentialLines_AST.java
@@ -0,0 +1,32 @@
+package ast_compare;
+
+public class SequentialLines_AST {
+
+	public void test1(){
+		int i = 0;
+	}
+	
+	public void test2(){
+		int i = 0;
+		int k = 0;
+	}
+	
+	public void test3(){
+		int i = 0; int b = 1;
+	}
+	
+	public void test5(double l){
+		int i = 0; int b = 1;
+	}
+	
+	public void test6(double l){
+		int i = 0; 
+		
+		int b = 1;
+	}
+	
+	public void test7(double l){
+		int i = 0; 
+		int b = 1;
+	}
+}
