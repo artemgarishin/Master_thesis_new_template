5a0860f8057a94306a49621fa26e7322947538a4
artemgarishin <artem.garishin@gmail.com>
2014-09-17 18:07:46 +0200

    java source code started

---
+++ TestSets/CompareTest/src/branchingsLines/Flowchart.java
@@ -0,0 +1,16 @@
+package branchingsLines;
+
+public class Flowchart {
+
+	public void flowchart(){
+		int i = 0;
+		boolean b = true;
+		if(i < 0){
+			System.out.println("Hello code");
+			i--;
+		}
+		else {
+			i++;
+		}
+	}
+}
