c4f099d330c44e863723e2ce7fb5a9544258092d
artemgarishin <artem.garishin@gmail.com>
2014-11-06 19:19:55 +0100

    java byte code started

--- TestSets/CompareTest/src/byteCodeCompare/ExampleByteCode1.java
+++ TestSets/CompareTest/src/byteCodeCompare/ExampleByteCode1.java
@@ -1,23 +1,28 @@
 package byteCodeCompare;
 
-import java.util.Random;
-
 public class ExampleByteCode1 {
 
 	public void main(String arg[]){
 		
-		int i = 0;
-		i = getnumber();
-		if(i > 5){
-			System.out.println("i > 5");
-		}
-		else{
-			System.out.println("i < 5");
+	}
+	
+	public void byteCode1(int i){
+		String s = null;
+		if(i > 20){
+			i++;
+			s = s + "more";
 		}
 	}
 	
-	public int getnumber(){
-		Random r = new Random();
-		return r.nextInt(10);
+	public void byteCode2(double i){
+		String str = null;
+		str = "first";
+		if(i > 20){
+			str = str + "more";
+			i++;
+		}
 	}
+	
+	
+	
 }
